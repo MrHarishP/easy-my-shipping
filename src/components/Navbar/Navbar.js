650ed80221d9495fea61f1ef9b3b3450fbecc41a
Harish pandey <mrharish.work@gmail.com>
2024-08-21 11:55:48 +0530

    ease my shipping

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = () => {
     <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Easy my shipping</a>
+    <a className="navbar-brand" href="/">EASE MY SHIPPING</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
